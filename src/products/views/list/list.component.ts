2e57dc8b60e4eda225aab39a04dfadb783daf8d4
Yanis <yanis971.60@gmail.com>
2025-03-11 11:15:40 +0100

    Feat : product delete

--- src/products/views/list/list.component.ts
+++ src/products/views/list/list.component.ts
@@ -1,8 +1,9 @@
 import {Component, inject} from '@angular/core';
-import {map, Observable} from 'rxjs';
+import {map, Observable, switchMap} from 'rxjs';
 import {ActivatedRoute, RouterLink} from '@angular/router';
 import {Produit} from '../../../app/views/home/home.component';
 import {AsyncPipe} from '@angular/common';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-list',
@@ -15,4 +16,10 @@ import {AsyncPipe} from '@angular/common';
 })
 export class ListComponent {
   products: Observable<Produit[]>  = inject(ActivatedRoute).data.pipe(map(({produits}) => produits || []))
+  private readonly http = inject(HttpClient)
+
+  delete(id: number) {
+    this.products = this.http.delete("/products/" + id)
+      .pipe(switchMap(() => this.http.get<Produit[]>("/products")))
+  }
 }
