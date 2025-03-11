08a4b2b7b28e888663d7c9ed9dc5d23fc2781c30
Yanis <yanis971.60@gmail.com>
2025-03-11 12:33:16 +0100

    Feat : auth response

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,19 +1,56 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {BehaviorSubject, map, tap} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private readonly http: HttpClient) { }
+  private readonly authResponse : BehaviorSubject<AuthResponse | undefined> = new BehaviorSubject<AuthResponse | undefined>(undefined)
+
+  get isLogged(): boolean {
+    return !!this.authResponse.value
+  }
+
+  get currentUser() {
+    return this.authResponse.value?.user
+  }
+
+  get token() {
+    return this.authResponse.value?.accessToken
+  }
+
+  get isAdmin() {
+    return this.currentUser?.admin
+  }
+
+  private readonly AUTH_KEY = "AUTH_RESPONSE"
+
+  constructor(private readonly http: HttpClient) {
+    const sessionResponse = sessionStorage.getItem(this.AUTH_KEY)
+    if(sessionResponse)
+      this.authResponse.next(JSON.parse(sessionResponse))
+
+
+    this.authResponse.subscribe(response => {
+      if(response) {
+        sessionStorage.setItem(this.AUTH_KEY, JSON.stringify(response))
+      } else {
+        sessionStorage.clear()
+      }
+    })
+  }
 
   login(data: LoginData) {
     return this.http.post<AuthResponse>("/login", data)
+      .pipe(tap(response => {
+        this.authResponse.next(response)
+      }))
   }
 
   logout() {
-
+    this.authResponse.next(undefined)
   }
 
   register(data: User) {
@@ -24,7 +61,6 @@ export class AuthService {
 export interface LoginData {
   email: string
   password: string
-
 }
 
 export interface User extends LoginData {
